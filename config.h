4ca1ff6fea57e8ee4ec83bfa903f1e7aa4b2fce0
adrian <thenewmailingadress@gmail.com>
2021-07-01 12:36:12 -0500

    update time to 24h format

--- config.h
+++ config.h
@@ -69,5 +69,5 @@ static const struct arg args[] = {
 	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
-	{ datetime, "%s",           "%a %b %d %r" },
+	{ datetime, "%s",           "%a %b %d %R" },
 };
